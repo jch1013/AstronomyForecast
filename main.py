062cbd7e2bdff57d4f8610137adf29c4a64763db
Jackson Hayward <jchayward1@gmail.com>
2022-08-29 17:35:53 -0700

    finish calling planet functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from datetime import date
 import getPlanets as planets
-import planet
+import getMoon as moon
 import DSO
 
+today = date.today().strftime("%Y-%m-%d")
+zipcode = 98115
 
-all_planets = planets.all
-for data in all_planets:
-    print(data)
-    print('\n')
+
+#all_planet_data = planets.get_planets(zipcode, today)
+moon_data = moon.get_moon(zipcode, today)
+
+
+
+
