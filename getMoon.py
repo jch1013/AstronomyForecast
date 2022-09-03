b4f70047b9b5209d620650742621d68806601d88
Jackson Hayward <jchayward1@gmail.com>
2022-09-03 11:16:20 -0700

    formatting code

--- getMoon.py
+++ getMoon.py
@@ -1,13 +1,12 @@
 import bs4
 from urllib.request import Request, urlopen
 
+
 def request_page(link):
     page_request = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(page_request).read()
     html_content = bs4.BeautifulSoup(webpage, "html.parser")
     return html_content
-
-
 
 
 def get_data(html):
@@ -21,7 +20,7 @@ def get_data(html):
 
 
 def get_moon(zipcode, today):
-    link =f'https://www.almanac.com/astronomy/moon-rise-and-set/zipcode/{zipcode}/{today}'
+    link = f'https://www.almanac.com/astronomy/moon-rise-and-set/zipcode/{zipcode}/{today}'
     moon_html = request_page(link)
     moon_array = get_data(moon_html)
     return moon_array
