2d8687cfba3708b8783f5932114f2a59f7fd71fa
Jackson Hayward <jchayward1@gmail.com>
2022-09-08 10:32:01 -0700

    modified to accept passed html

--- getPlanets.py
+++ getPlanets.py
@@ -1,28 +1,4 @@
-import bs4
-from urllib.request import Request, urlopen
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import planet
-
-
-# requests HTML content from original webpage
-# returns cleaned HTML content
-
-def request_page(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "lightgreybox"))
-        )
-    finally:
-        # driver.quit()
-        pass
-    html = driver.page_source
-    html_content = bs4.BeautifulSoup(html, "html.parser")
-    return html_content
 
 
 # collects data from HTML and creates planet objects
@@ -40,21 +16,7 @@ def get_data(html):
     return planets
 
 
-# can accept zipcode input from main and calls all functions
 # returns list of all planet objects
-def get_planets(coordinates, today, timezone):
-    link = make_link(coordinates, today, timezone)
-    planet_html = request_page(link)
-    all_planets = get_data(planet_html)
+def get_planets(html):
+    all_planets = get_data(html)
     return all_planets
-
-
-def make_link(coordinates, today, timezone):
-    lat = coordinates[0]
-    long = coordinates[1]
-    year = today[0]
-    month = today[1]
-    day = today[2]
-    return f'https://in-the-sky.org/whatsup_times.php?year={year}&month={month}&day={day}&latitude={lat}&longitude={long}&timezone={timezone}'
-
-
