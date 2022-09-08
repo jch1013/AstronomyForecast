9544c922c1bf4ede58051ca4d300dc9bc1c486bf
Jackson Hayward <jchayward1@gmail.com>
2022-09-08 11:45:55 -0700

    update moon function to return report from coordinates

--- getMoon.py
+++ getMoon.py
@@ -1,26 +1,26 @@
-import bs4
-from urllib.request import Request, urlopen
 
 
-def request_page(link):
-    page_request = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(page_request).read()
-    html_content = bs4.BeautifulSoup(webpage, "html.parser")
-    return html_content
+def get_moon_data(html):
+    rise_html = html.select(".lightgreybox")[0]
+    illumination_html = html.select(".lightgreybox")[2]
+    moon_rise_set = rise_html.findAll(lambda tag: tag.name == 'tr')[2]
+    moon_array = []
+    for time in moon_rise_set:
+        moon_array.append(time.text)
+    rise = moon_array[3]
+    apex = moon_array[5]
+    sets = moon_array[7]
+    illumination = illumination_html.findAll(lambda tag: tag.name == 'p')[1].text
+    illumination = illumination.replace('\n', "").strip()
+    return [rise, sets, apex, illumination]
 
 
-def get_data(html):
-    data = html.select(".rise_highlight")
-    all_moon_data = data[0].findAll(lambda tag: tag.name == 'td')
-    rise = all_moon_data[0].text
-    apex = all_moon_data[1].text
-    sets = all_moon_data[3].text
-    moon_array = [rise, apex, sets]
-    return moon_array
-
-
-def get_moon(zipcode, today):
-    link = f'https://www.almanac.com/astronomy/moon-rise-and-set/zipcode/{zipcode}/{today}'
-    moon_html = request_page(link)
-    moon_array = get_data(moon_html)
-    return moon_array
+def get_moon(html):
+    data = get_moon_data(html)
+    report = "The Moon:"
+    moonrise = f'Rise time: {data[0]}'
+    moonset = f'Set time: {data[1]}'
+    apex = f'Apex time: {data[2]}'
+    illumination = f'Illumination: {data[3]}'
+    report = report + '\n' + moonrise + '\n' + moonset + '\n' + apex + '\n' + illumination + '\n'
+    return report
