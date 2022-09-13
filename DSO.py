e8fa393400144683285201b5a27c09226e846738
Jackson Hayward <jchayward1@gmail.com>
2022-09-13 10:51:38 -0700

    remove direction, type, and magnitude attributes

--- DSO.py
+++ DSO.py
@@ -1,12 +1,9 @@
-class DSO:
-    def __init__(self, name, rise, set, apex, direction, type, magnitude):
+class dso:
+    def __init__(self, name, rise, sets, apex):
         self.name = name
         self.rise = rise
-        self.set = set
+        self.sets = sets
         self.apex = apex
-        self.direction = direction
-        self.type = type
-        self.magnitude = magnitude
 
     def __str__(self):
-        return f"Object: {self.name}\nType: {self.type}\nMagnitude: {self.magnitude}\nRise time: {self.rise}\nSet time: {self.set}\nApex: {self.apex}\nDirection: {self.direction}"
+        return f"Object: {self.name}\nRise time: {self.rise}\nSet time: {self.sets}\nApex: {self.apex}\n"
