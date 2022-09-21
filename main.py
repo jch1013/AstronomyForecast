41eb6345af8edba783d41dc31810d9dbe67e27fe
Jackson Hayward <jchayward1@gmail.com>
2022-09-21 11:42:37 -0700

    Automatically respoond to emails

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import getWeather
 import makeReport
 import emailFunctions as email
 import readEmails
 import coordinates
-
+import sendEmails
 
 
 
@@ -15,18 +16,25 @@ test_location = [47.6, -122.3]
 def main():
     # get all emails currently in the inbox
     service = email.gmail_authenticate()
-    results = email.search_messages(service, "lemon")
+    results = email.search_messages(service, "miami")
 
     # get subject and sender from every email in inbox
-    locations_and_users = []
     for mail in results:
         data = readEmails.read_message(service, mail)
-        locations_and_users.append(data)
+        location = data[0]
+        destination = data[1]
+        coords = coordinates.lat_long(data[0])
+        report_body = makeReport.make_report(coords[0], coords[1])
+        sendEmails.send_message(service, destination, "Forecast for " + location, report_body)
 
-    print(locations_and_users)
-    print("\nNumber of emails found: " + str(len(locations_and_users)))
 
-
-
+    print("\nNumber of emails found: " + str(len(results)))
 
 main()
+
+
+
+
+
+
+# -----Tests-----
