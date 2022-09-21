02975ae1f473cb9a8b2c8653e4070ebe65b28989
Jackson Hayward <jchayward1@gmail.com>
2022-09-21 11:00:07 -0700

    Added in error handling

--- getWeather.py
+++ getWeather.py
@@ -6,13 +6,19 @@ coordinates and the Beautiful Soup library to process and extract the data. Data
 import bs4
 import requests
 
+
 # This function returns the forecast at a given location for tonight
 def get_forecast(lat, lng):
     link = f'https://forecast.weather.gov/MapClick.php?lon={lng}&lat={lat}'
+    print(link)
     page = requests.get(link)
     html = bs4.BeautifulSoup(page.text, "html.parser")
 
-    daytime = (html.select('.col-sm-2.forecast-label')[0].text == "Today")
+    try:
+        daytime = (html.select('.col-sm-2.forecast-label')[0].text != "Tonight")
+    except:
+        return "Unable to provide weather data at this time. Please try again later"
+
     if daytime:
         forecast = html.select('.col-sm-10.forecast-text')[1].text
     else:
